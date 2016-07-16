53666c9caff125aea5d230fb9b7dcd7c224e322c
Jiechao Wang <jiechao@starmobileinc.com>
2016-07-16 17:14:16 -0400

    StartActivityForResult / Photo

--- app/src/main/java/com/example/myapplication/activity/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/activity/ProfileActivity.java
@@ -66,7 +66,7 @@ public class ProfileActivity extends AppCompatActivity {
         ListView mDrawerList = (ListView)findViewById(R.id.navList);
         ArrayAdapter<String> mAdapter;
         // TODO: turn these into enum
-        String[] activities = {"Parcel", "Sorting", "LayoutParams", "Notification/Pending Intent", "Custom View", "Gesture", "Video/Activity Callbacks", "Simple Adapter/Long List", "Spinner", "Tweet Timeline", "Tweet View", "Twitter Login", "Snackbar", "Inflater", "App Bar", "Loader", "Dialog", "Recycler", "Tabbed", "Custom List", "Tasks", "Animation", "Cache", "Database", "Music Play", "My List", "Broadcast", "Grid",
+        String[] activities = {"Photo/For Result", "Parcel", "Sorting", "LayoutParams", "Notification/Pending Intent", "Custom View", "Gesture", "Video/Activity Callbacks", "Simple Adapter/Long List", "Spinner", "Tweet Timeline", "Tweet View", "Twitter Login", "Snackbar", "Inflater", "App Bar", "Loader", "Dialog", "Recycler", "Tabbed", "Custom List", "Tasks", "Animation", "Cache", "Database", "Music Play", "My List", "Broadcast", "Grid",
                 "Screen Size", "REST", "Gson", "Map", "Content" };
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, activities);
         mDrawerList.setAdapter(mAdapter);
@@ -79,6 +79,9 @@ public class ProfileActivity extends AppCompatActivity {
                 String text = ((TextView)view).getText().toString();
 
                 switch (text) {
+                    case "Photo/For Result":
+                        startActivity(new Intent(ProfileActivity.this, PhotoActivity.class));
+                        break;
                     case "Parcel":
                         startActivity(new Intent(ProfileActivity.this, ParcelActivity.class));
                         break;
